64922afcb92a96ee76742ffbdda26a3defcadca4
theJoseNeto <joseneto180201@gmail.com>
2021-08-09 15:57:22 -0300

    barra para regular porcentagem de uso

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ function convertHourToMinutes(time) {
    return time * 60;
 }
 
-function reduceTimeIn80Percent(time) {
+function reduceTimeIn80Percent(time, percent) {
    const minutes = convertHourToMinutes(time);
-   const result = (minutes * 80) / 100;
+   const result = (minutes * percent) / 100;
    return result;
 
 }
 
 document.addEventListener('submit', function (e) {
    e.preventDefault();
-   const appName = document.querySelector('#app').value;
+   const appName = document.querySelector('#apps').value;
+   const percent = document.querySelector('#bar').value;
    const timeUse = document.querySelector('#timeUse').value;
+
    const result = document.querySelector('.result');
-   const resultMin = parseFloat(reduceTimeIn80Percent(Number(timeUse) / 60).toFixed(2));
-   result.innerHTML += `Seu tempo de uso no ${appName} foi reduzido em 80% e agora você tem ${resultMin}h para estudar  e ${(timeUse - resultMin).toFixed(2)}h para ${appName}`;
+
+   const resultMin = parseFloat(reduceTimeIn80Percent(timeUse / 60, percent).toFixed(2));
+
+   result.innerHTML += `Seu tempo de uso no ${appName} foi reduzido em ${percent}% e agora você tem ${resultMin}h para estudar e ${(timeUse - resultMin).toFixed(2)}h para usar o ${appName} <br/> <br/>`;
 });
+
+
+
+
